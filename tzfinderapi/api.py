f8e5a95666f846206675124a7c0e0ab4d78c11e2
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-05 14:08:41 +0000

    Add addition /tz endpoint with same usage as tz-at

--- tzfinderapi/api.py
+++ tzfinderapi/api.py
@@ -18,5 +18,10 @@ def tz_at(query: Coordinates):
     return Timezone(timezone_id=tf.timezone_at(lat=query.lat, lng=query.lng))
 
 
+@app.post("/tz", response_model=Timezone)
+def tz(query: Coordinates):
+    return Timezone(timezone_id=tf.timezone_at(lat=query.lat, lng=query.lng))
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
