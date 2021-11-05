c838d73373aaea0a6eb2ab7c6127701051def13d
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-05 14:09:00 +0000

    Add test for /tz endpoint

--- tests/test_tzfinderapi.py
+++ tests/test_tzfinderapi.py
@@ -8,3 +8,9 @@ def test_tz_at(client):
     response = client.post("/tz-at", json={"lat": 51.5, "lng": -0.11})
     assert response.status_code == 200
     assert response.json() == {"timezone_id": "Europe/London"}
+
+
+def test_tz(client):
+    response = client.post("/tz-at", json={"lat": 51.5, "lng": -0.11})
+    assert response.status_code == 200
+    assert response.json() == {"timezone_id": "Europe/London"}
