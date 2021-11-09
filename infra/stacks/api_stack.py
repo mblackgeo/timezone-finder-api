1072b0252b0bcf1607dc7fb1bd966456e9858eb6
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-09 16:23:52 +0000

    Pass the apigw stage into the container as an env var and explicitly set to prod

--- infra/stacks/api_stack.py
+++ infra/stacks/api_stack.py
@@ -7,14 +7,18 @@ class RestfulApiGatewayStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        opts = apigw.StageOptions(stage_name="prod")
+
         fn = _lambda.DockerImageFunction(
             self,
             "RestfulApiFxn",
             code=_lambda.DockerImageCode.from_image_asset(".."),
+            environment={"STAGE": opts.stage_name},
         )
 
         api = apigw.LambdaRestApi(  # noqa: F841
             self,
             "TestRoute",
             handler=fn,
+            deploy_options=opts,
         )
