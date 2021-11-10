63b79e0f75bbcae642dc08308d797732a8c46b0b
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2021-11-10 11:23:49 +0000

    Update .env and reinstate config handling

---
+++ infra/stacks/config.py
@@ -0,0 +1,14 @@
+import os
+from types import SimpleNamespace
+
+from dotenv import load_dotenv
+
+# Load variables from .env
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
+
+
+conf = SimpleNamespace(
+    aws_region=os.environ["AWS_REGION"],
+    aws_account=os.environ["AWS_ACCOUNT"],
+    domain_name=os.environ["DOMAIN_NAME"],
+)
